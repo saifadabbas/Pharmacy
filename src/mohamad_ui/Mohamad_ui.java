6f2e25c0a6c7196efd78ead721514c4e7f747d7f
mohammadIsa <mohamad.isa.moghrabi@gmail.com>
2022-04-19 02:15:46 +0300

    intial design

---
+++ src/mohamad_ui/Mohamad_ui.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mohamad_ui;
+
+/**
+ *
+ * @author pc
+ */
+public class Mohamad_ui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
